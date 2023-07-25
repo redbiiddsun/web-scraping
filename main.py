7eb9ba0673c092f897e800ad835acb13ddfc47ed
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-07-25 16:47:31 +0700

    commit

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
-
+import json
 
 url = "https://www.youtube.com/@mojiko.official/shorts"
 
 res = requests.get(url)
 res.encoding = "utf-8"
-print(res)
 
+# if res.status_code == 200:
+#     print("Successful")
+# elif res.status_code == 404:
+#     print("Error 404 page not found")
+# else:
+#     print("Not both 200 and 404")
 
-if res.status_code == 200:
-    print("Successful")
-elif res.status_code == 404:
-    print("Error 404 page not found")
-else:
-    print("Not both 200 and 404")
-
-
-soup = BeautifulSoup(res.text, 'html.parser')
+temp = ""
+soup = BeautifulSoup(res.text, "html.parser")
 text = soup.findAll('script')
-print(text[35])
-# for i in text: 
-    
-#     print(i)
-#     print("----------------------------------------------------------------------------------------")
+
+for i in text[35]:
+    temp = i
+
+temp = temp.rsplit('var ytInitialData = ')
+temp2 = temp[1].rsplit(';', 1)
+
+json_obj = json.loads(temp2[0])
+
+youtube_content = json_obj['contents']['twoColumnBrowseResultsRenderer']['tabs'][2]['tabRenderer']['content']['richGridRenderer']['contents']
+
+for i in youtube_content:
+    try:
+        print(i['richItemRenderer']['content']['reelItemRenderer']['videoId'])
+        print(i['richItemRenderer']['content']['reelItemRenderer']['headline']['simpleText'])
+        print(i['richItemRenderer']['content']['reelItemRenderer']['thumbnail']['thumbnails'][0]['url'])
+    except:
+        continue
+
+
