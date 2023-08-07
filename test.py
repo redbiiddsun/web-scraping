9cd9cb5b957bb42dac56d6c2c7a2d1771b19405f
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-08-07 22:49:44 +0700

    update script

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import scrapetube
 from pytube import YouTube
 from pytube.exceptions import AgeRestrictedError, VideoUnavailable
 import threading
-import concurrent.futures
 
 
 error = []
@@ -10,61 +9,66 @@ error = []
 
 def getVideoID(channelURL, content_type):
     videoId = []
-    videos = scrapetube.get_channel(channel_url='https://www.youtube.com/@mojiko.official', content_type='shorts')
-    for video in videos:
-        videoId.append(video['videoId'])
-    return videoId
+    try:
+        videos = scrapetube.get_channel(channel_url = channelURL, content_type = content_type)
+        for video in videos:
+            videoId.append(video['videoId'])
+        return videoId
+    except Exception as e:
+        print("Youtub Channel link does not exists")
+        exit()
+    
 
 def downloadVideo(link):
     try:
         youtubeObject = YouTube(link)
         youtubeObject = youtubeObject.streams.get_highest_resolution()
-        youtubeObject.download()
-    except:
-        print("An error has occurred")
-        error.append(link)
-    print("Download is completed successfully")
+        youtubeObject.download('Downloads')
+    except Exception as e:
+        print(f"{link} :Error {e}")
+        error.append(f"{link} :Error {e}")
+    print(f"Download {link} is completed successfully")
 
 
-# for i in range(0, len(list)): 
-#     downloadVideo('http://youtube.com/watch?v=' + list[i])
-
-def jobPrepare(lists):
+def threadTask(lists):
     for i in range(0, len(lists), 10): 
         yield lists[i:i + 10]
 
 def threadDownload(lists):
     for i in range(0, len(lists)):
-        print(i)
+        # print(i)
         downloadVideo('http://youtube.com/watch?v=' + lists[i])
 
-# def createThread(list):
-    
-
-if __name__ =="__main__":
-    lists = getVideoID("channelURL", "content_type")
-
-    x = list(jobPrepare(lists))
-
-    # # # creating thread
-    # t1 = threading.Thread(target=threadDownload, args=(x[0],))
-    # t2 = threading.Thread(target=threadDownload, args=(x[1],))
- 
-    # # # starting thread 1
-    # t1.start()
-    # # # starting thread 2
-    # t2.start()
- 
-    # # # wait until thread 1 is completely executed
-    # t1.join()
-    # # # wait until thread 2 is completely executed
-    # t2.join()
- 
-    # # # both threads completely executed
-    # print("Done!")
-
+def executeThread(videoID_list):
     thread_list = []
-    for i in range(0, len(x)):
-        thread = threading.Thread(target=threadDownload, args=(x[i],))
+
+    for i in range(0, len(videoID_list)):
+        thread = threading.Thread(target=threadDownload, args=(videoID_list[i],))
         thread_list.append(thread)
         thread.start()
+    
+    for i in thread_list:
+        i.join()
+
+    
+
+
+    
+    
+if __name__ =="__main__":
+
+    while 1:
+
+        link = str(input('Youtube Channel URL: '))
+
+        while (link == ''):
+            link = str(input('Youtube Channel URL: '))
+
+
+        videoID = getVideoID(link, 'shorts')
+
+        videoID_list = list(threadTask(videoID))
+
+        executeThread(videoID_list)
+        
+        print("--------------------------------------------------------------------------")
